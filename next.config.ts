a3cdafa44d39c8589f42030c700ef7ba2c89e886
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-22 20:23:53 +0530

    ibid

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,8 @@
 import type { NextConfig } from "next";
-import withMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  output: "export", // Enable static export
-  webpack: (config, { isServer }) => {
-    // Suppress infrastructure warnings
-    config.infrastructureLogging = {
-      level: "error",
-    };
-
-    // Add any additional custom Webpack config here
-    return config;
-  },
-  // Page extensions to include .md and .mdx files
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  // Example rewrites
-  async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: "/learning/weekly-reflections/week-:num",
-          destination: "/learning/weekly-reflections/weeks/week-:num",
-        },
-      ],
-    };
-  },
 };
 
-// Export with MDX support
-export default withMDX({
-  extension: /\.mdx?$/, // Support .mdx files
-})(nextConfig);
+export default nextConfig;
