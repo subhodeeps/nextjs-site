68957a040b320f378a19b2cc63f90cd659bcac1d
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-22 21:01:02 +0530

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: "export", // Enables static export
   /* config options here */
   reactStrictMode: true,
 };
