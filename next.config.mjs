c87b3d1c021ba0a8433b7155c284290b8985cad9
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-22 20:50:59 +0530

    some deletions

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-import createMDX from '@next/mdx';
-
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Required for static export
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], // Include MDX and TSX
-};
-
-const withMDX = createMDX();
-
-export default withMDX(nextConfig);
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  
+}
+ 
+const withMDX = createMDX({
+})
+ 
+export default withMDX(nextConfig)
