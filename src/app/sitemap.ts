c0bf47e79f15ca950d6c62568e036869e96b29a2
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-24 10:40:59 +0530

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next'
+
+
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://subhodeeps.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://subhodeeps.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    // Add more routes as needed
+  ]
+}
