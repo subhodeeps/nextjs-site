ab7b34ec555bd1bf82dcb920422890b36ba2fb66
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-24 10:08:43 +0530

    google verification attempt 4

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
+   
     title: 'In the time of chimpanzees I was a monkey | subhodeep',
     description: 'I don\'t really know what I\'m doing',
+    verification: { 
+    google: "JfvlM1XeQizWzQrb51mAuBNk0wpUeRdKKxJ9Voq0BHc" },
     openGraph: {
         title: 'In the time of chimpanzees I was a monkey | subhodeep',
         description: 'I don\'t really know what I\'m doing',
@@ -12,8 +15,9 @@ export const metadata: Metadata = {
         card: 'summary_large_image',
         title: 'In the time of chimpanzees I was a monkey | subhodeep',
         description: 'I don\'t really know what I\'m doing',
-    },
-    verification: { google: "JfvlM1XeQizWzQrb51mAuBNk0wpUeRdKKxJ9Voq0BHc" }
+    }
+
+   
 }
 
 export default function Layout({
